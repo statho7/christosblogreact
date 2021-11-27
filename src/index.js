41cac3f5a97febcc6d05f7c9b136e867a8dd244b
statho7 <andreas.stathop@gmail.com>
2021-11-27 23:40:24 +0000

    minor modifications

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import './index.css';
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from 'redux-persist/integration/react'
+import { store } from "./redux/store";
+// import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    {/* <PersistGate loading={null}> */}
       <App />
-    </PersistGate>
+    {/* </PersistGate> */}
   </Provider>,
   document.getElementById("root")
 );
