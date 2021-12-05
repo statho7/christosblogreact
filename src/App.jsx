fafbd417bff7717ef29905889a4cd97e90378d99
statho7 <andreas.stathop@gmail.com>
2021-12-05 19:21:45 +0000

    Lazy loading in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css';
-import Home from './pages/Home';
 import Navbar from './components/Navbar';
-import Article from './pages/Article';
-import Category from './pages/Category';
+import Home from './pages/Home';
+// import Article from './pages/Article';
+// import Category from './pages/Category';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   // Redirect,
 } from "react-router-dom";
-import Author from './pages/Author';
+// import Author from './pages/Author';
+import { Suspense, lazy } from 'react';
+
+const Author = lazy(() => import('./pages/Author'));
+const Article = lazy(() => import('./pages/Article'));
+const Category = lazy(() => import('./pages/Category'));
 
 const App = () => {
   return (
     <Router>
+    <Suspense fallback={<div>Loading...</div>}>
       <Navbar />
       <Switch>
         <Route path="/article/:id">
@@ -29,6 +35,7 @@ const App = () => {
           <Home />
         </Route>
       </Switch>
+      </Suspense>
     </Router>
   );
 }
